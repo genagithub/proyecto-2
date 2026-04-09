5ffd2cffa542cd7c1d355d98ae91e795a76f837b
genagithub <143031972+genagithub@users.noreply.github.com>
2026-04-09 13:26:06 -0300

    commit

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ server = app.server
 app.layout = html.Div(id="body", className="e2_body", children=[
     html.A(href="https://github.com/genagithub/proyecto-2/edit/main/optimizaci%C3%B3n_de_inversi%C3%B3n_publicitaria.ipynb", children=[html.H1("Estrategia de Inversión: del volumen a la rentabilidad", className="e2_title")]),
     
-    html.Div(style={"display":"flex","justify-content":"space-around","padding":"20px","margin-bottom":"15px"}, children=[
+    html.Div(className="e2_div_stats", children=[
         html.Div([html.H3("Margen Neto Promedio:"), html.B(f"${round(mean_margin, 2)}",style={"font-weight":"bold","font-size":"1em"})], className="e2_stats"),
         html.Div([html.H3("Elasticidad TV (dominancia):"), html.B(f"{round(elasticities["TV"], 2)}",style={"font-weight":"bold","font-size":"1em"})], className="e2_stats"),
         html.Div([html.H3("Elasticidad Radio (oportunidad):"), html.B(f"{round(elasticities["Radio"], 2)}",style={"font-weight":"bold","font-size":"1em"})], className="e2_stats"),
@@ -36,8 +36,8 @@ app.layout = html.Div(id="body", className="e2_body", children=[
                 dcc.Graph(id="graph-bar", className="e2_graphs", figure={})
         ]),
         
-        html.Div(style={"width":"60%","margin-top":"40px"}, children=[
-               html.Label("Simulador de Rebalanceo: Mover presupuesto de TV a Radio (%)", style={"color":"black","font-weight":"bold","margin-bottom": "10px", "display": "block"}),
+        html.Div(className="e2_div_slider", children=[
+               html.Label("Simulador de Rebalanceo: Mover presupuesto de TV a Radio (%)", className="e2_label"),
                dcc.Slider(
                    id="rebalance-slider", 
                    min=0, max=30, step=5, value=0, 
@@ -48,7 +48,7 @@ app.layout = html.Div(id="body", className="e2_body", children=[
 
         html.Div(className="e2_column_2", children=[
             dcc.Graph(id="graph-scatter", figure={}),
-            html.Div(id="text-resolution", style={"padding": "15px", "background-color": "#f9f9f9", "border-radius": "10px"})
+            html.Div(id="text-resolution", classsName="e2_text_resolution")
         ])
     ])
 ])
